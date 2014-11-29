571b0a5bc89ddc19b79affd061da5b5632267f49
Ohui A Kwao <afi.kwao@meltwater.org>
2014-11-29 15:41:19 +0000

    results showing in student

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer',  platforms: :ruby
 
 gem 'pry'
+gem 'pry-byebug'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,7 @@ class StudentsController < ApplicationController
   # GET /students/1
   # GET /students/1.json
   def show
+    @results = @student.results
   end
 
   # GET /students/new
